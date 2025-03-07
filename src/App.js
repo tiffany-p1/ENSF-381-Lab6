a35e7652f7c0f2fc6d67fea7c53f54dff0f3025f
Tiffany P <tiffany__pham@outlook.com>
2025-03-07 11:50:08 -0700

    Completed ex1

--- src/App.js
+++ src/App.js
@@ -2,22 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const currentYear = new Date().getFullYear();
+  let isLoggedIn = false;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>ENSF-381: Full Stack Web Development</h1>
+      <p>React Components</p>
+      <p>The current year is: {currentYear}</p>
+      {isLoggedIn ? <p>Welcome back!</p> : <p>Please log in.</p>}
     </div>
   );
 }
