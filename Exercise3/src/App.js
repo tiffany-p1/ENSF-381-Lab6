95d065da203ad56bcddf4005e6101c0f9617f5db
Tiffany P <tiffany__pham@outlook.com>
2025-03-07 12:35:51 -0700

    Copied src from exercise3 branch to Exercise3 folder in main branch

---
+++ Exercise3/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import EngineeringTopics from "./EngineeringTopics"
+
+function App() {
+  return(
+  <div>
+    <h2>Engineering Topics</h2>
+    <EngineeringTopics />
+  </div>
+  );
+}
+
+
+export default App; 
